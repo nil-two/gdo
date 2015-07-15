8522f4fd0edb168292031352165cfd84b3d951be
wara <kusabashira227@gmail.com>
2015-07-16 00:21:50 +0900

    Add test for Processor.Process

--- gdo_test.go
+++ gdo_test.go
@@ -66,3 +66,25 @@ func TestNewProcessor(t *testing.T) {
 			name, actual, expect)
 	}
 }
+
+func TestProcess(t *testing.T) {
+	name, arg := "sed", "s/false/true/g"
+	if _, err := exec.LookPath(name); err != nil {
+		t.Skipf("%q: doesn't exist", name)
+	}
+	p, err := NewProcessor(name, arg)
+	if err != nil {
+		t.Errorf("NewProcessor(%q, %q) returns %q, want nil",
+			name, arg, err)
+	}
+
+	expect := []string{"true", "true", "true", "nil"}
+	actual := []string{"true", "false", "false", "nil"}
+	if err = p.Process(actual); err != nil {
+		t.Errorf("NewProcessor(%q, %q).Process(%q) returns %q, want nil",
+			name, arg, actual, err)
+	}
+	if !reflect.DeepEqual(actual, expect) {
+		t.Errorf("got %q, want %q", actual, expect)
+	}
+}
