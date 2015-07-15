392781a9b520d0491d0b8b8e6767508ede6f5664
wara <kusabashira227@gmail.com>
2015-07-15 22:29:10 +0900

    Add test for Matcher.MatchString

--- gdo_test.go
+++ gdo_test.go
@@ -24,3 +24,28 @@ func TestNewMatcher(t *testing.T) {
 		t.Errorf("got %q, want %q", actual, expect)
 	}
 }
+
+func TestMatch(t *testing.T) {
+	expr := `\d+`
+	m, err := NewMatcher(expr)
+	if err != nil {
+		t.Errorf("NewMatcher(%q) returns %q, want nil",
+			expr, err)
+	}
+
+	src1 := "xxx123"
+	expect1 := true
+	actual1 := m.MatchString(src1)
+	if expect1 != actual1 {
+		t.Errorf("MatchString(%q) = %v, want %v",
+			expr, actual1, expect1)
+	}
+
+	src2 := "xxxabc"
+	expect2 := false
+	actual2 := m.MatchString(src2)
+	if expect2 != actual2 {
+		t.Errorf("MatchString(%q) = %v, want %v",
+			expr, actual2, expect2)
+	}
+}
