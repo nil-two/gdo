e6122b15d60acb7ca836c23b0b304cf15f56fc2c
wara <kusabashira227@gmail.com>
2015-07-16 21:18:14 +0900

    Implement usage for now

--- main.go
+++ main.go
@@ -9,3 +9,10 @@ func guideToHelp() {
 Try 'gdo --help' for more information.
 `[1:])
 }
+
+func usage() {
+	os.Stderr.WriteString(`
+Usage: gdo [OPTION]... PATTERN COMMAND [ARGS]...
+Process matched lines with COMMAND
+`[1:])
+}
