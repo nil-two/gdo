f5eee4bf88bcb48f92564e9301653b6d8c68e728
wara <kusabashira227@gmail.com>
2015-07-17 00:09:31 +0900

    Add test for ParseOption

--- gdo_test.go
+++ gdo_test.go
@@ -94,6 +94,25 @@ func TestProcess(t *testing.T) {
 	}
 }
 
+func TestParseOption(t *testing.T) {
+	args := []string{`\d+`, "perl", "-ple", "$_*=2", "--", "foo", "bar"}
+	expect := &Option{
+		IsHelp:  false,
+		Pattern: `\d+`,
+		Command: "perl",
+		Arg:     []string{"-ple", "$_*=2"},
+		Files:   []string{"foo", "bar"},
+	}
+	actual, err := ParseOption(args)
+	if err != nil {
+		t.Errorf("ParseOption(%q) returns %q, want nil",
+			args, err)
+	}
+	if !reflect.DeepEqual(actual, expect) {
+		t.Errorf("got %q, want %q", actual, expect)
+	}
+}
+
 func TestNewLines(t *testing.T) {
 	expect := &Lines{
 		lines:          []string{},
