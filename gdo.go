59dbc429c506c977843d46a5e6196dcef9dc5b64
wara <kusabashira227@gmail.com>
2015-07-17 21:18:53 +0900

    Extract Matcher from gdo.go

--- gdo.go
+++ gdo.go
@@ -7,25 +7,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os/exec"
-	"regexp"
 )
-
-type Matcher struct {
-	re *regexp.Regexp
-}
-
-func NewMatcher(expr string) (m *Matcher, err error) {
-	m = &Matcher{}
-	m.re, err = regexp.Compile(expr)
-	if err != nil {
-		return nil, err
-	}
-	return m, nil
-}
-
-func (m *Matcher) MatchString(s string) bool {
-	return m.re.MatchString(s)
-}
 
 type Processor struct {
 	cmd *exec.Cmd
---
+++ matcher.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"regexp"
+)
+
+type Matcher struct {
+	re *regexp.Regexp
+}
+
+func NewMatcher(expr string) (m *Matcher, err error) {
+	m = &Matcher{}
+	m.re, err = regexp.Compile(expr)
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (m *Matcher) MatchString(s string) bool {
+	return m.re.MatchString(s)
+}
